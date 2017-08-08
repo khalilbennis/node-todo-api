f3cad5672224c963f69bd437fadbaf6bfa31974a
khalil bennis <khaliloxmp@gmail.com>
2017-08-08 21:25:33 +0100

    initial app todo api

---
+++ playground/mongodb-find.js
@@ -0,0 +1,22 @@
+const {MongoClient,ObjectID} = require('mongodb');
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+  if (err) {
+    return console.log('unable to connect to mongodb');
+  }
+//  db.collection('Todos').find({
+//    _id:new ObjectID('598a13cbe76ffe53d080539f')
+//
+//  }).toArray().then((docs)=>{
+  //  console.log('Todos');
+  //  console.log(JSON.stringify(docs,undefined,2));
+//  },(err)=>{
+  //  console.log('unable to fetch');
+  //});
+  db.collection('Todos').find().count().then((count)=>{
+   console.log(`todos coun:${count}`);
+
+  },(err)=>{
+     console.log('unable to fetch');
+  });
+
+});
